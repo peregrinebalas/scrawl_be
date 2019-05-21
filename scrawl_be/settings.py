12d1fa5a1c579ae5c665c110afcb1a4dbe7fe6d2
Scott Thomas <smthom05@gmail.com>
2019-05-20 18:52:43 -0600

    Changes port and postgres version
    Co-authored-by: Peregrine Balas <39976770+PeregrineReed@users.noreply.github.com>

--- scrawl_be/settings.py
+++ scrawl_be/settings.py
@@ -80,6 +80,8 @@ DATABASES = {
         'NAME': 'scrawl_database',
         'USER': 'postgres',
         'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
         'TEST': {
             'NAME': 'scrawl_test_db'
         }
