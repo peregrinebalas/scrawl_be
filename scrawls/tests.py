0961e17049d8aa6eb9fdf611baac42789abc2929
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-05-21 14:12:38 -0600

    Adds test for POST to Walls

--- scrawls/tests.py
+++ scrawls/tests.py
@@ -6,13 +6,22 @@ from django.urls import reverse
 
 from .models import Wall, Comment
 
-class WallModelTests(TestCase):
+class PostWallTests(TestCase):
 
-<<<<<<< HEAD
-=======
-class WallModelTests(TestCase):
+    def test_a_wall_can_be_added(self):
+        response = self.client.post(reverse('scrawls:create'), {
+            'name': 'Turing School of Software & Design',
+            'address': '1331 17th Street, Denver, CO, USA',
+            'lat': 25.3454567,
+            'long': 90.1234567,
+            'radius': 0.0101234
+        })
+        self.assertEqual(response.status_code, 201)
+        self.assertContains(response, {'Turing School of Software & Design can now be found at 1331 17th Street, Denver, CO, USA'})
 
->>>>>>> 224b672bb8de6ee21981ffcdb8978a1ef4e9b325
+
+class ModelTests(TestCase):
+
     def test_it_exists(self):
         wall = Wall.objects.create(name='Wall', address='100 St.', lat=3.5, lng=3.5, range=3.5)
         all = Wall.objects.all()
