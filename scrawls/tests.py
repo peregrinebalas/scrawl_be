e5d8958a38e22e48d66f5b7a8e2c0911c869e60c
Scott Thomas <smthom05@gmail.com>
2019-05-23 15:25:47 -0600

    Add dynamic test with serializer

--- scrawls/tests.py
+++ scrawls/tests.py
@@ -9,6 +9,8 @@ from rest_framework.test import APITestCase, APIClient
 from rest_framework.views import status
 
 from .models import Wall, Comment
+from .serializers import WallsSerializer
+
 
 client = APIClient()
 class PostWallTests(APITestCase):
@@ -32,7 +34,7 @@ class PostWallTests(APITestCase):
 class WallShowTests(APITestCase):
 
     def setUp(self):
-        self.Turing = Wall.objects.create(
+        self.turing = Wall.objects.create(
             name='Turing School of Software & Design', address='1331 17th Street, Denver, CO, USA', lat=25.3454567, lng=90.1234567, range=0.0101234)
 
         self.comment_1 = Comment.objects.create(
@@ -40,10 +42,11 @@ class WallShowTests(APITestCase):
 
     def test_it_can_get_a_specific_wall(self, **kwargs):
         url = reverse('scrawls:wall-show', kwargs={'pk': self.turing.pk})
-        wall = Wall.objects.get(pk=kwargs['pk'])
-
-        response = client.get(url, wall, content_type='application/json')
+        wall = Wall.objects.get(pk=self.turing.pk)
+        serializer = WallsSerializer(wall)
+        response = client.get(url, serializer.data, content_type='application/json')
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, serializer.data)
 
 class ModelTests(TestCase):
 
