ef0dc2292ead1e1c7c1c0c32567df38cb5f4f393
Scott Thomas <smthom05@gmail.com>
2019-05-23 13:32:14 -0600

    Updates test to grab specific data

--- scrawls/tests.py
+++ scrawls/tests.py
@@ -29,26 +29,21 @@ class PostWallTests(APITestCase):
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.data, {'message': 'Turing School of Software & Design can be found at 1331 17th Street, Denver, CO, USA.'})
 
-# class WallShowTests(APITestCase):
-#
-#     def test_it_can_get_a_specific_wall(self, **kwargs):
-#         url = reverse('scrawls:wall-show')
-#         data = json.dumps(
-#             {
-#                 'name': 'Turing School of Software & Design',
-#                 'address': '1331 17th Street, Denver, CO, USA',
-#                 'lat': 25.3454567,
-#                 'lng': 90.1234567,
-#                 'range': 0.0101234,
-#                 'comments': {
-#                     "comment": "I'm a comment"
-#                     }
-#             }
-#         )
-#
-#         response = client.get(url, data=data, content_type='application/json')
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(response.data, {'message': 'Turing School of Software & Design can be found at 1331 17th Street, Denver, CO, USA.'})
+class WallShowTests(APITestCase):
+
+    def setUp(self):
+        self.Turing = Wall.objects.create(
+            name='Turing School of Software & Design', address='1331 17th Street, Denver, CO, USA', lat=25.3454567, lng=90.1234567, range=0.0101234)
+
+        self.comment_1 = Comment.objects.create(
+            wall= self.turing, comment='Turing School is a place')
+
+    def test_it_can_get_a_specific_wall(self, **kwargs):
+        url = reverse('scrawls:wall-show', kwargs={'pk': self.turing.pk})
+        wall = Wall.objects.get(pk=kwargs['pk'])
+
+        response = client.get(url, wall, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
 
 class ModelTests(TestCase):
 
