4f8aafdfe92ac4cea4f62b65fe5f18b91868c3c7
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-05-25 23:07:54 -0600

    updated settings for geodjango

--- scrawl_be/settings.py
+++ scrawl_be/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     'rest_framework',
     'corsheaders',
 ]
@@ -79,7 +80,7 @@ WSGI_APPLICATION = 'scrawl_be.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'scrawl_database',
         'USER': 'postgres',
         'PASSWORD': '',
